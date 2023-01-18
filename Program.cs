7cb5ececded802fc780b66647ec33a5bcca4def9
bzofkie <122408688+bzofkie@users.noreply.github.com>
2023-01-17 21:49:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+
+            string stg = "Hello, World! (2)";
+            Console.WriteLine("Solution (2): " + stg);
+
+            Console.WriteLine("Enter the user's name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + userName);
+
+            Console.WriteLine("Enter the user's age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + age);
+
+        }
+    }
+}
